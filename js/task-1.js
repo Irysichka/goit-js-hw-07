dcfbfab6312928b1f1226ab557a29b7331a99ced
Irina <Irysichka2704@gmail.com>
2025-05-21 20:31:07 +0300

    add

---
+++ js/task-1.js
@@ -0,0 +1,22 @@
+const item = document.querySelectorAll(".item");
+console.log(`Number of categories: ${item.length}`);
+
+const titleAnimals = document.querySelector(".title-animals");
+console.log(`Category: ${titleAnimals.textContent}`)
+
+const animalList = document.querySelectorAll(".animal-list-item");
+console.log(`Elements: ${animalList.length}`);
+
+
+const titleProducts = document.querySelector(".title-products");
+console.log(`Category: ${titleProducts.textContent}`)
+
+const foodList = document.querySelectorAll(".food-list-item");
+console.log(`Elements: ${foodList.length}`);
+
+
+const titleTechnologies = document.querySelector(".title-technologies");
+console.log(`Category: ${titleTechnologies.textContent}`)
+
+const itList = document.querySelectorAll(".it-list-item");
+console.log(`Elements: ${itList.length}`);
